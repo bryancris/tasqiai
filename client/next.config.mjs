4ff4ea29ba75fe0f809ec61bb4efd1f40c3feb19
Bryan McAfee <bryan.m@quraishimd.com>
2024-10-25 16:37:04 -0500

    Initial Commit

---
+++ client/next.config.mjs
@@ -0,0 +1,42 @@
+import nextMDX from '@next/mdx';
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  output: 'standalone',
+  serverRuntimeConfig: {
+    hostname: '0.0.0.0',
+  },
+  experimental: {
+    mdxRs: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '**',
+      },
+    ],
+  },
+};
+
+export default withMDX(nextConfig);
---
+++ server/Context/ApplicationContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Models;
+using Npgsql.EntityFrameworkCore;
+
+namespace server.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+
+        public DbSet<server.Models.Task>? Tasks { get; set; }
+
+        public DbSet<server.Models.Subtask>? Subtasks { get; set; }
+
+        public DbSet<server.Models.RecurringTask>? RecurringTasks { get; set; }
+
+        public DbSet<server.Models.List>? Lists { get; set; }
+
+        public DbSet<server.Models.Label>? Labels { get; set; }
+
+        public DbSet<server.Models.Project>? Projects { get; set; }
+    }
+}
